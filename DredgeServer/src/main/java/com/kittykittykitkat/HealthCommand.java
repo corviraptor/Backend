9883f2a5cdb5e2a78d5b4626e1195946a7c138cd
KittyKittyKitKat <simon.palmer03@gmail.com>
2024-08-07 17:06:54 -0400

    Big Resourcepack Update

---
+++ DredgeServer/src/main/java/com/kittykittykitkat/HealthCommand.java
@@ -0,0 +1,4 @@
+package com.kittykittykitkat;
+
+public class HealthCommand {
+}
---
+++ DredgeServer/src/main/java/com/kittykittykitkat/mixin/HungerManagerAccessor.java
@@ -0,0 +1,10 @@
+package com.kittykittykitkat.mixin;
+import net.minecraft.entity.player.HungerManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(HungerManager.class)
+public interface HungerManagerAccessor {
+    @Accessor("saturationLevel")
+    void setSaturationLevel(float saturationLevel);
+}
